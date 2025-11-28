e9cf67389feb061676ba3eed90049f7e5bf2fde1
Sameer Suresh Kanade <sameerka@cybage.com>
2025-11-28 12:02:35 +0530

    modified:backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # return jsonify(message="Hello from Flask Backend", service="backend")
-    return jsonify(message="Hello from CI/CD! Sameer", service="backend")
+    return jsonify(message="Hello from CI/CD! Sameer Suresh Kanade", service="backend")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
